ca1abe03d7fc9a598ebb2b4f0a1e129c1874a2db
Edric <edric.matviev@gmail.com>
2020-01-03 21:40:27 +0800

    Rolling back change.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,6 +51,3 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-# ReadTheDocs does not support the building of modules with C-library dependencies.
-autodoc_mock_imports = ["mpi4py"]
